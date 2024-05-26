1c5ef8bad7fd8c91824960735c2861251e0d9628
Christian Toribio <chris09toribio@gmail.com>
2024-05-26 11:43:03 -0700

    Added drawer for detailed tasks setting.

--- app/components/task.jsx
+++ app/components/task.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 export default function Task({ description }) {
     return (
-        <div className="w-full flex gap-2  items-center">
+        <div className="w-full flex gap-2 items-center rounded-lg p-4 bg-white drop-shadow-lg">
             <input type="checkbox" name="item" />
-            <label>todo</label>:<p>{description ?? "missing description"}</p>
+            <p>{description ?? "missing description"}</p>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@
 
 import { useState } from "react";
 import Task from "./components/task";
+import { GoPlus } from "react-icons/go";
 
 export default function Home() {
     const [tasks, setTasks] = useState([]);
     const [currentTask, setCurrentTask] = useState("");
+    const [isDrawerActive, setDrawerActive] = useState(false);
 
     const handleChange = (event) => {
         if (event.key === "Enter") handleSubmit();
@@ -21,9 +23,22 @@ export default function Home() {
     };
 
     return (
-        <main className="w-full h-full flex flex-col justify-center items-center">
-            <h1 className="p-4 text-lg underline">Todo List</h1>
-            <div className="w-full px-4 mb-6 flex gap-4">
+        <main className="w-full h-screen flex flex-col">
+            <div className="w-full flex justify-center items-center px-4">
+                <div className="w-1/3"></div>
+                <h1 className="w-1/3 p-4 text-lg underline flex flex-col items-center text-nowrap">
+                    Todo List
+                </h1>
+                <div className="w-1/3 justify-end flex">
+                    <GoPlus
+                        className="scale-150 text-gray-700"
+                        onClick={() => {
+                            setDrawerActive(!isDrawerActive);
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="w-full px-4 my-2 flex gap-4 justify-center items-center">
                 <input
                     className="border-2 p-2 rounded-lg"
                     placeholder="enter todo item"
@@ -38,7 +53,7 @@ export default function Home() {
                     submit
                 </button>
             </div>
-            <div className="w-full flex flex-col justify-center items-center px-8 py-4 bg-blue-50">
+            <div className="w-full h-fit flex flex-col items-center px-8 py-4 mt-4 mb-16 bg-blue-50 gap-2 overflow-auto hover:overflow-scroll">
                 {tasks.length === 0 ? (
                     <p className="text-gray-500">task list is empty</p>
                 ) : (
@@ -49,6 +64,30 @@ export default function Home() {
                         />
                     ))
                 )}
+            </div>
+            <div
+                className={`fixed w-full ${
+                    isDrawerActive
+                        ? "pointer-events-all"
+                        : "pointer-events-none"
+                }`}
+            >
+                <div
+                    className={`w-screen h-screen bg-black ${
+                        isDrawerActive ? "opacity-50" : "opacity-0"
+                    } transition-opacity duration-500`}
+                    onClick={() => {
+                        setDrawerActive(false);
+                    }}
+                ></div>
+                <div
+                    className={`fixed w-full h-5/6 bottom-0 bg-white flex flex-col items-center rounded-t-xl ${
+                        isDrawerActive ? "translate-y-0" : "translate-y-full"
+                    } transition-transform duration-500`}
+                >
+                    <div className="w-12 h-2 bg-gray-300 rounded-lg my-2"></div>
+                    <h1 className="">add a tasks here</h1>
+                </div>
             </div>
         </main>
     );
